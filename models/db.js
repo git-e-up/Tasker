1ad54fc5d7fe629eddc21d77bfa5120cada3c237
Matt Lissner <mlissne@gmail.com>
2015-05-28 11:06:33 -0500

    tasks underway

---
+++ models/db.js
@@ -0,0 +1,19 @@
+var mongoose = require ('mongoose');
+
+
+var connectionString = "mongodb://localhost/tasker";
+
+mongoose.connect(connectionString);
+
+mongoose.connection.on('connected', function() {
+  console.log('[MongoDB]: Connected bisque');
+});
+
+mongoose.connection.on('disconnected', function() {
+  console.log('[MongoDB]: Disconnected!');
+});
+
+mongoose.connection.on('error', function(error) {
+  console.log('[MongoDB]: Error, see below');
+  console.log(error);
+});
